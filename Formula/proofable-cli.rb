2591d508e6ef61e013ddeec166f2fdf1fcd1f245
Guan Gui <root@guiguan.net>
2020-07-14 23:03:21 +1000

    Improve test

--- Formula/proofable-cli.rb
+++ Formula/proofable-cli.rb
@@ -15,6 +15,13 @@ class ProofableCli < Formula
   end
 
   test do
-    assert_equal "proofable-cli version #{version}", shell_output("#{bin}/proofable-cli --version").strip
+    cert_path = "cli.proofable"
+    # `proofable-cli` should:
+    # 1. be able to create a blockchain certificate of itself
+    shell_output("#{bin}/proofable-cli create proof \"#{bin}/proofable-cli\" -p \"#{cert_path}\" "\
+                 "-t HEDERA --include-metadata --dev-token=magic "\
+                 "--api.host-port=\"api.dev.proofable.io:443\"")
+    # 2. verify the certificate. Here the cli should remember last token and API hostport
+    shell_output("#{bin}/proofable-cli verify proof \"#{bin}/proofable-cli\" -p \"#{cert_path}\"")
   end
 end
