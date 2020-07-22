929aa09637c35f6d5b45e759f607464ac264e079
Guan Gui <root@guiguan.net>
2020-07-22 15:01:39 +1000

    Upgrade to v0.2.5

--- Formula/proofable-cli.rb
+++ Formula/proofable-cli.rb
@@ -1,8 +1,8 @@
 class ProofableCli < Formula
   desc "Certifying any digital assets (files) to public blockchains"
   homepage "https://docs.proofable.io"
-  url "https://github.com/SouthbankSoftware/proofable/archive/v0.2.2.tar.gz"
-  sha256 "11e4e76cb2998168e047436fb9ef414b152e1513c80f1ee0d6093da815810c5e"
+  url "https://github.com/SouthbankSoftware/proofable/archive/v0.2.5.tar.gz"
+  sha256 "3c23d181e982c2b99b12af781cce5b3bd6da0d397e1fcd7b9a2a6bf36cfb3127"
   license "AGPL-3.0"
   head "https://github.com/SouthbankSoftware/proofable.git"
 
